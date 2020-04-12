0758dd83d053a062f2a6dd549fafac669967c5f3
Diana Wallace <dianawallacedeveloper@gmail.com>
2020-04-11 19:34:29 -0600

    added event listener for preloader

---
+++ app.js
@@ -0,0 +1,8 @@
+// hide preloader
+// all the images scripts links have finished loading
+
+// window event listener
+
+window.addEventListener('load', function () {
+  document.querySelector('.preloader').style.display = "none";
+})
